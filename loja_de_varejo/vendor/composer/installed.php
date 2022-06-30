8d955499633dfcd48e05c4207818bc15a940ef10
LuGartie <107418170+LuGartie@users.noreply.github.com>
2022-06-30 11:55:06 -0300

    Principio da função Fornecedor

---
+++ loja_de_varejo/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e6d14386f9dd968df7a6371fe67128854deb661b',
+        'name' => 'anderson/loja_de_varejo',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'anderson/loja_de_varejo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e6d14386f9dd968df7a6371fe67128854deb661b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
